116a4fc6d0e1e539b12d1bc4eb93bccc9ec32415
sakakuraurluru <y_sakakura@uluru.jp>
2020-09-04 17:57:32 +0900

    sleep(120)

--- routes/api.php
+++ routes/api.php
@@ -12,11 +12,11 @@
 */
 
 Route::post('/payment/EntryTran.idPass', function () {
-    sleep(14);
+    // sleep(14);
     return 'AccessID=4bdef2f1c811e1d8f3895ac4b2e8fdd9&AccessPass=5225917879478d7c15364dee20d84322';
 });
 
 Route::post('/payment/ExecTran.idPass', function () {
-    sleep(14);
+    sleep(120);
     return 'ACS=0&OrderID=200903034615-001-00002976&Forward=2a99662&Method=1&PayTimes=&Approve=0337650&TranID=2009031509111111111111815783&TranDate=20200903154618&CheckString=7b6d735a296d798f94142aaa0ca1ccbb';
 });
