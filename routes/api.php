cdecdc49558742144d1427e246f52965d780f6c9
sakakuraurluru <y_sakakura@uluru.jp>
2020-09-04 18:16:44 +0900

    getを追加

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,8 @@ Route::post('/payment/ExecTran.idPass', function () {
     sleep(120);
     return 'ACS=0&OrderID=200903034615-001-00002976&Forward=2a99662&Method=1&PayTimes=&Approve=0337650&TranID=2009031509111111111111815783&TranDate=20200903154618&CheckString=7b6d735a296d798f94142aaa0ca1ccbb';
 });
+
+Route::get('/payment/ExecTran.idPass', function () {
+    sleep(120);
+    return 'ACS=0&OrderID=200903034615-001-00002976&Forward=2a99662&Method=1&PayTimes=&Approve=0337650&TranID=2009031509111111111111815783&TranDate=20200903154618&CheckString=7b6d735a296d798f94142aaa0ca1ccbb';
+});
