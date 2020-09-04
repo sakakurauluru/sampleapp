5014e4271390b72ee872ab098b952a27d3e0f199
sakakuraurluru <y_sakakura@uluru.jp>
2020-09-04 11:32:46 +0900

    ExecTran.idPassのルーティング、レスポンスを設定した。

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +11,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/payment/EntryTran.idPass', function (Request $request) {
-    return 'アイウエオ';
+Route::get('/payment/EntryTran.idPass', function () {
+    return 'AccessID=4bdef2f1c811e1d8f3895ac4b2e8fdd9&AccessPass=5225917879478d7c15364dee20d84322';
+});
+
+Route::get('/payment/ExecTran.idPass', function () {
+    return 'ACS=0&OrderID=200903034615-001-00002976&Forward=2a99662&Method=1&PayTimes=&Approve=0337650&TranID=2009031509111111111111815783&TranDate=20200903154618&CheckString=7b6d735a296d798f94142aaa0ca1ccbb';
 });
