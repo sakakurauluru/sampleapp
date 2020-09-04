c1932e697191834b293e07c1a11228666a6824d8
sakakuraurluru <y_sakakura@uluru.jp>
2020-09-04 11:45:06 +0900

    get→postに変更

--- routes/api.php
+++ routes/api.php
@@ -11,10 +11,10 @@
 |
 */
 
-Route::get('/payment/EntryTran.idPass', function () {
+Route::post('/payment/EntryTran.idPass', function () {
     return 'AccessID=4bdef2f1c811e1d8f3895ac4b2e8fdd9&AccessPass=5225917879478d7c15364dee20d84322';
 });
 
-Route::get('/payment/ExecTran.idPass', function () {
+Route::post('/payment/ExecTran.idPass', function () {
     return 'ACS=0&OrderID=200903034615-001-00002976&Forward=2a99662&Method=1&PayTimes=&Approve=0337650&TranID=2009031509111111111111815783&TranDate=20200903154618&CheckString=7b6d735a296d798f94142aaa0ca1ccbb';
 });
