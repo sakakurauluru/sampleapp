6c4b47709bbb51e802bc7f2d2536ca4104dee822
sakakuraurluru <y_sakakura@uluru.jp>
2020-09-09 16:52:07 +0900

    最大値に設定

--- routes/api.php
+++ routes/api.php
@@ -13,19 +13,19 @@
 
 // 取引登録
 Route::post('/payment/EntryTran.idPass', function () {
-    sleep(0);
+    sleep(28);
     return 'AccessID=4bdef2f1c811e1d8f3895ac4b2e8fdd9&AccessPass=5225917879478d7c15364dee20d84322';
 });
 
 // カード検索
 Route::post('/payment/SearchCard.idPass', function () {
-    sleep(0);
+    sleep(28);
     return 'CardSeq=0&DefaultFlag=0&CardName=&CardNo=*************881&Expire=3005&HolderName=MARUKO SHUFU&DeleteFlag=0';
 });
 
 // 決済登録
 Route::post('/payment/ExecTran.idPass', function () {
-    sleep(0);
+    sleep(88);
     return 'ACS=0&OrderID=200903034615-001-00002976&Forward=2a99662&Method=1&PayTimes=&Approve=0337650&TranID=2009031509111111111111815783&TranDate=20200903154618&CheckString=7b6d735a296d798f94142aaa0ca1ccbb';
 });
 
